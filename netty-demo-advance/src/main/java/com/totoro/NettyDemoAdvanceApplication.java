8d0e86926c11545caadf504df54dc7346116e7f7
totoro <1026215903@qq.com>
2022-11-16 18:10:00 +0800

    虫洞栈

---
+++ netty-demo-advance/src/main/java/com/totoro/NettyDemoAdvanceApplication.java
@@ -0,0 +1,13 @@
+package com.totoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyDemoAdvanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyDemoAdvanceApplication.class, args);
+    }
+
+}
