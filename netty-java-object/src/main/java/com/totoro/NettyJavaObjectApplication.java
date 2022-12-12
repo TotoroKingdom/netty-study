0ab206feafb6507f3a9b619cc6ce8dd110b75acd
totoro <1026215903@qq.com>
2022-12-12 18:11:59 +0800

    java object

---
+++ netty-java-object/src/main/java/com/totoro/NettyJavaObjectApplication.java
@@ -0,0 +1,13 @@
+package com.totoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyJavaObjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyJavaObjectApplication.class, args);
+    }
+
+}
