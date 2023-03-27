4420eb9e728912c8d19f894d80a6436429aae661
totoro <1026215903@qq.com>
2023-03-27 17:55:40 +0800

    synchronous-communication-ClientSocket

--- netty-synchronous-communication/src/main/java/com/totoro/client/ClientSocket.java
+++ netty-synchronous-communication/src/main/java/com/totoro/client/ClientSocket.java
@@ -1,9 +1,63 @@
 package com.totoro.client;
 
+import com.totoro.codec.RpcDecoder;
+import com.totoro.codec.RpcEncoder;
+import com.totoro.msg.Request;
+import com.totoro.msg.Response;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
 /**
  * @author:totoro
  * @createDate:2023/3/27
  * @description:
  */
-public class ClientSocket {
+public class ClientSocket implements Runnable {
+
+    private ChannelFuture future;
+
+    @Override
+    public void run() {
+
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(workerGroup);
+            b.channel(NioSocketChannel.class);
+            b.option(ChannelOption.AUTO_READ, true);
+            b.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel channel) throws Exception {
+                    channel.pipeline().addLast(
+                            new RpcDecoder(Response.class),
+                            new RpcEncoder(Request.class),
+                            new MyClientHandler());
+                }
+            });
+
+            ChannelFuture f = b.connect("127.0.0.1", 7379).sync();
+            this.future = f;
+            f.channel().closeFuture().sync();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+
+    public ChannelFuture getFuture(){
+        return future;
+    }
+
+    public void setFuture(ChannelFuture future){
+        this.future = future;
+    }
 }
--- netty-synchronous-communication/src/main/java/com/totoro/client/MyClientHandler.java
+++ netty-synchronous-communication/src/main/java/com/totoro/client/MyClientHandler.java
@@ -1,9 +1,30 @@
 package com.totoro.client;
 
+import com.totoro.future.SyncWriteFuture;
+import com.totoro.future.SyncWriteMap;
+import com.totoro.msg.Response;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
 /**
  * @author:totoro
  * @createDate:2023/3/27
  * @description:
  */
-public class MyClientHandler {
+public class MyClientHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object obj) throws Exception {
+        Response msg = (Response) obj;
+        String requestId = msg.getRequestId();
+        SyncWriteFuture future = (SyncWriteFuture) SyncWriteMap.syncKey.get(requestId);
+        if (future != null){
+            future.setResponse(msg);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
 }
